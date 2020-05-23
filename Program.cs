3d16a56b02ace4e53a4126bd359d3e81dea96893
ForeverThinking <35239092+ForeverThinking@users.noreply.github.com>
2020-05-23 21:12:05 +0100

    add method for computer to make choice

--- Program.cs
+++ Program.cs
@@ -14,6 +14,16 @@ namespace RockPaperScissors
             Console.WriteLine("Welcome to Rock, Paper, Scissors.");
             Console.Write("Please make your choice: ");
             userChoice = Console.ReadLine();
+
+            computerChoice =  ComputerMakeChoice(choices);
+        }
+
+        private static string ComputerMakeChoice(string[] movesToChooseFrom)
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(movesToChooseFrom.Length);
+            string computerAnswer = movesToChooseFrom[randomIndex];
+            return computerAnswer;
         }
     }
 }
